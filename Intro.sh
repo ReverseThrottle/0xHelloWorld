a9b7ca8615696e9037d310ad5402cd087e3830ad
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2020-09-15 16:55:22 -0700

    Learning the basics

--- Intro.sh
+++ Intro.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
+#declaring variables and there values
 student=ReverseThrottle
 school=University of Advancing Technology
 class=ShellScripting
 
+#Using my variables
 echo "My name is $student , I am currently attending $school and studing $class
