07d8e0907fe01147e2986259e53e91312c5d5e59
ReverseThrottle <54719088+ReverseThrottle@users.noreply.github.com>
2020-09-15 16:53:32 -0700

    Create Intro.sh

---
+++ Intro.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+student=ReverseThrottle
+school=University of Advancing Technology
+class=ShellScripting
+
+echo "My name is $student , I am currently attending $school and studing $class
